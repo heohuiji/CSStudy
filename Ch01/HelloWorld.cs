e2cff0b51c817553fe28f42133d0d5fe7e6e96c0
heohuiji <newlife0727@naver.com>
2022-07-13 13:19:32 +0900

    2022/07/12 기본예제

---
+++ Ch01/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022년 07월 12일
+ * 이름:허희지
+ * 내용: C# 개발환경 설정, Hello World 출력 교재 p44
+ */
+namespace Ch01
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //기본출력
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
+
+            //서식출력
+            Console.Write("Hello\t");
+            Console.Write("Korea\n");
+
+
+            //포맷출력
+            Console.WriteLine("{0}, {1}", "Hello", "Busan");
+        }
+
+    }
+}
