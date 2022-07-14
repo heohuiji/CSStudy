2c25f4501c84b5dcbe187a525f5c1b2e3ebb5679
heohuiji <newlife0727@naver.com>
2022-07-14 17:13:46 +0900

    2022/07/14 exam

---
+++ Exam/01/01_02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exam._01
+{
+    internal class _01_02
+    {
+        static void Main2(string[] args)
+        {
+            int year;
+            int birth;
+            string name;
+
+            Console.Write("올해 년도 입력 : ");
+            year = int.Parse(Console.ReadLine()); //readline은 문자라서 int로 변환
+
+            Console.Write("태어난 년도 입력 : ");
+            birth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("이름 입력 : ");
+            name = Console.ReadLine();
+
+            int age = year - birth;
+
+            Console.Write("{0}님 안녕하세요. \n당신은 올해 만 {1}세 입니다.", name, age);
+
+
+            
+        }
+    }
+}
